a8d9ccadafff904e63eb32ac605c8fc5d129587c
hiramotoys <hiramoto.ys@gmail.com>
2011-02-14 20:06:10 +0900

    指定したディレクトリ以下のPNGファイルを全部読み込むようにした

--- script/TestCscSensor.py
+++ script/TestCscSensor.py
@@ -4,8 +4,14 @@ import ctypes
 
 argvs = sys.argv
 argc = len(argvs)
+print argvs
 if (argc != 2):
     print 'Usage: # python %s filename' % argvs[0]
     sys.exit()
 lib = ctypes.CDLL('../test/CrossStripesCameraSensor/libcscsensor.dylib')
-lib.imageprocessing(ctypes.c_char_p("a"), ctypes.c_char_p("b"))
+files = glob.glob(argvs[1] + '/*.png')
+counter = 0
+for f in files:
+    writename = 'Result/' + '%05d'%counter + '.png'
+    lib.imageprocessing(ctypes.c_char_p(f), ctypes.c_char_p(writename))
+    counter += 1
